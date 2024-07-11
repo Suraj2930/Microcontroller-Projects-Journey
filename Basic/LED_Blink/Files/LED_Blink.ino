95ab3ba2fd8e9cb2363ba2f40fcc299265dc0a21
Suraj Yadav <75577969+Suraj2930@users.noreply.github.com>
2024-07-11 22:32:12 +0530

    Documented
    Added comments where needed

--- Basic/LED_Blink/Files/LED_Blink.ino
+++ Basic/LED_Blink/Files/LED_Blink.ino
@@ -1,14 +1,14 @@
-const int LedPin = 0;
+const int LedPin = 0; // defining LedPin to pin 0
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode (LedPin, OUTPUT);
+  pinMode(LedPin, OUTPUT); // setting pin as output
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LedPin, HIGH);
-  delay(1000);
-  digitalWrite(LedPin, LOW);
-  delay(1000);
+  digitalWrite(LedPin, HIGH); // set pin state to high (1)
+  delay(1000);                // wait for one second
+  digitalWrite(LedPin, LOW);  // set pin state to low (0)
+  delay(1000);                // wait for one second
 }
